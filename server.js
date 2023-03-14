693c04000770818cca249a38908b4f9a5a4a38a5
faranbutt <faranbutt789@gmail.com>
2023-03-14 17:08:53 +0500

    Updated KNEX

--- server.js
+++ server.js
@@ -12,14 +12,10 @@ const image = require('./controllers/Image')
 const db =  knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString : process.env.DATABASE_URL,
     ssl:true
   }
 });
-
-
-
-
 
 app.use(bodyParser.json());
 app.use(cors());
